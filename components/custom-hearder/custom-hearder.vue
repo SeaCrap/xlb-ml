b8880cdd3f906b08adf6822678cd0f57762ce159
lanmoxia <506076395@qq.com>
2023-06-11 17:07:24 +0800

    头部基础完成

---
+++ components/custom-hearder/custom-hearder.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="custom-header">
+		<view class="shop-bimage">
+			<image class="bgimg" src="../../static/images/bg.jpg" mode="aspectFill"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "custom-hearder",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.custom-header {
+		overflow: hidden;
+		background: #eecccf;
+		height: 300rpx;
+
+		.shop-bimage {
+			width: 100%;
+			height: 100%;
+
+			.bgimg {
+				width: 100%;
+				height: 100%;
+				filter: blur(30rpx);
+				transform: scale(30rpx);
+			}
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,15 @@
 <template>
-	<view class="content">
-		<u-button text="月落"></u-button>
+	<view class="wrapper">
+		<custom-hearder />
+		<view class="content">
+			<view class="infoModel">
+				<view class="delivery">免费配送</view>
+				<view class="my-order">
+					<u-icon name="calendar" color="#576b95" size="23"></u-icon>
+					我的订单
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -11,5 +20,33 @@
 </script>
 
 <style lang="scss">
-	
+	.wrapper {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		.content {
+			flex: 1;
+			background: $border-color-light;
+			border-radius: 20rpx 20rpx 0 0;
+			margin-top: -20rpx;
+			position: relative;
+			z-index: 2;
+			overflow: hidden;
+			.infoModel {
+				@include flex-box()
+				color: $text-font-color-1;
+				height: 100rpx;
+				background: #fff;
+				padding: 0 30rpx;
+				.delivery {}
+				.my-order {
+					@include flex-box()
+					color: $brand-theme-color-aux;
+					.calendar {
+						
+					}
+				}
+			}
+		}
+	}
 </style>
