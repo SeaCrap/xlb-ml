82ccf1fa09504277983848b7c93e4dcbc552d2bb
lanmoxia <506076395@qq.com>
2023-07-11 20:28:29 +0800

    商家信息布局样式

--- pages_manage/brand/brand.vue
+++ pages_manage/brand/brand.vue
@@ -1,6 +1,38 @@
 <template>
-	<view>
-		商家信息
+	<view class="brand">
+		<uni-forms 
+			ref="brandRef" 
+			:model="brandFromData" 
+			:rules="brandRules"
+			:label-width="100"
+			label-align="right">
+			
+			<uni-forms-item label="品牌 Logo">
+				<uni-file-picker 
+					v-model="brandFromData.thumb" 
+					fileMediatype="image" 
+					mode="grid" 
+					:limit="1"/>
+			</uni-forms-item>
+			
+			<uni-forms-item label="品牌名称" name="name" required>
+				<uni-easyinput type="text" v-model="brandFromData.name" placeholder="请输入品牌名" />
+			</uni-forms-item>
+			
+			<uni-forms-item label="商户电话" name="mobile" required>
+				<uni-easyinput type="text" v-model="brandFromData.mobile" placeholder="请输入商户电话" />
+			</uni-forms-item>
+			
+			<uni-forms-item label="商户地址" name="address">
+				<uni-easyinput v-model="brandFromData.address" placeholder="请输入商家地址" />
+			</uni-forms-item>
+			
+			<uni-forms-item label="商家介绍" name="about">
+				<uni-easyinput type="textarea" v-model="brandFromData.about" placeholder="请输入商家介绍" />
+			</uni-forms-item>
+			
+			<button type="primary" @click="onSubmit">提交信息</button>
+		</uni-forms>
 	</view>
 </template>
 
@@ -8,12 +40,68 @@
 	export default {
 		data() {
 			return {
-				
-			};
+				thumb: [],
+				brandFromData: {
+					name: "",
+					mobile: "",
+					address: "",
+					about: ""
+				},
+				brandRules: {
+					name: {
+						rules: [
+							{
+								required: true,
+								errorMessage: "请输入品牌名称"
+							},
+							{
+								minLength: 3,
+								maxLength: 20,
+								errorMessage: '长度在 {minLength} 到 {maxLength} 的字符'
+							}
+						]
+					},
+					mobile: {
+						rules: [
+							{
+								required: true,
+								errorMessage: "请输入商户电话"
+							},
+							{
+								validateFunction:function(rule,value,data,callback){
+									let res = /^1[3-9]\d{9}$/.test(value)
+									if(!res){callback("手机号码格式不正确")}
+								},
+							}
+						]
+					},
+				}
+			}
+		},
+		methods: {
+			onSubmit(){
+				this.$refs.brandRef.validate().then(res => {
+					let filterImages = this.brandFromData.thumb.map(item => {
+						return {
+							extname: item.extname,
+							url: item.url,
+							name: item.name,
+							size: item.size
+						}
+					})
+					this.brandFromData.thumb = filterImages
+					this.addAndUpdate()
+				}).catch(err => {console.log(err)})
+			},
+			addAndUpdate(){
+				console.log(this.brandFromData)
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.brand {
+	padding: 30rpx;
+}
 </style>
